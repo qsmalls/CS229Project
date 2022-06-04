5d89c7d53629f6462890e02bbc07271218a53337
Tian <tian.26@hotmail.com>
2022-06-03 23:41:12 -0400

    New weather condition with random time per road

--- main.py
+++ main.py
@@ -60,6 +60,52 @@ def data_modification(data):
 
     data.to_csv('Crash_and_Road_and_Weather_Added.csv')
 
+
+def str_time_prop(start, end, time_format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formatted in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    """
+
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(time_format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%Y-%m-%d %H:%M', prop)
+
+
+def generated_new(data):
+    # pick a random time in between 07/01/2012 and 05/31/2022
+    list_1 = list()
+    for x in range(120613):
+        list_1.append(random_date("2012-07-01 00:00", "2022-05-31 23:59", random.random()))
+    data['timedate'] = list_1
+    data['timedate'] = pd.to_datetime(data['timedate'])
+    data['timedate_start'] = data['timedate'].transform(minus_30)
+    data['timedate_end'] = data['timedate'].transform(add_30)
+    data['weather_fetch'] = data['timedate_start'].astype(str) + ',' + data['timedate_end'].astype(str)
+
+    data['timedate'] = data['timedate'].astype(str)
+    data[['CRASH TIME', 'CRASH DATE']] = data.timedate.str.split(expand=True)
+
+    print(data)
+    try:
+        data.append(data['weather_fetch'].transform(get_weather))
+    except Exception as e:
+        print(e)
+    data = data.drop(['timedate', 'timedate_end', 'timedate_start', 'weather_fetch'], axis=1)
+    print('done')
+    data.to_csv('All_Roads_Without_Accidents.csv')
+
+
 def data_cleaning(data):
     # remove index
     data = data.iloc[: , 1:]
@@ -73,6 +119,7 @@ if __name__ == '__main__':
     pd.set_option('display.max_rows', None)
     #df = pd.read_csv('Crash_and_Road_and_Weather.csv', low_memory=False)
     #data_cleaning(df)
-    data_modification(pd.read_csv('Crash_and_Road_and_Weather_Cleaned.csv', low_memory=False))
+    #data_modification(pd.read_csv('Crash_and_Road_and_Weather_Cleaned.csv', low_memory=False))
+    generated_new(pd.read_csv('Centerline.csv', low_memory=False))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
